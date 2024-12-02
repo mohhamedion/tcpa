9e7628aada421f3cd0f38e8f89ca23416e5ab9aa
Administrator <mohhamedion@gmail.com>
2024-12-02 21:58:57 +0300

    Add foreign id to users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('login')->unique();
             $table->string('password');
-            $table->foreignId('company_id')->nullable()->references('id')->on('companies');
             $table->rememberToken();
             $table->timestamps();
         });
