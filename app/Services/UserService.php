ab70be13d154bcf6e8f625b1a41a137b389e079d
Administrator <mohhamedion@gmail.com>
2024-12-02 22:19:27 +0300

    Add company

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Enums\User\Roles;
+use App\Models\Company;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Throwable;
@@ -13,13 +14,19 @@ class UserService
     /**
      * @throws Throwable
      */
-    public function store(string $name, string $login, string $password, Roles $role)
+    public function store(string $name, string $login, string $password, Roles $role, Company $company = null)
     {
         $user = new User();
         $user->name = $name;
         $user->login = $login;
         $user->password = Hash::make($password);
         $user->assignRole($role->value);
+
+        if($company)
+        {
+            $user->company_id = $company->id;
+        }
+
         $user->saveOrFail();
     }
 
