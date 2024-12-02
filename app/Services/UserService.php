1f0964ba40507a4101f9dcb5bb4fabf402225380
Administrator <mohhamedion@gmail.com>
2024-12-02 22:02:38 +0300

    Create store method

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,9 +2,26 @@
 
 namespace App\Services;
 
+use App\Enums\User\Roles;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Throwable;
 
 class UserService
 {
+
+    /**
+     * @throws Throwable
+     */
+    public function store(string $name, string $login, string $password, Roles $role)
+    {
+        $user = new User();
+        $user->name = $name;
+        $user->login = $login;
+        $user->password = Hash::make($password);
+        $user->assignRole($role->value);
+        $user->saveOrFail();
+    }
 
 
 }
