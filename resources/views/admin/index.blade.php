1a1c059ac37a6f8e67ca0ddf036bd949755ec20c
Administrator <mohhamedion@gmail.com>
2024-12-03 18:10:16 +0300

    add doc

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,6 +4,12 @@
 
     <section class="container">
 
+        <h2>mini doc</h2>
+        <div class="alert alert-warning">
+            <p>On this page, you can find all created companies, and agents that belong to that company</p>
+            <a href="{{route('companies.index')}}">Companies</a>
+        </div>
+
     </section>
 
 @endsection
