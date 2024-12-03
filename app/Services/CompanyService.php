1ede509274647f96de25b01d19e6e9f29c37927a
Administrator <mohhamedion@gmail.com>
2024-12-03 16:22:52 +0300

    Add update and delete

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -2,19 +2,43 @@
 
 namespace App\Services;
 
-
 use App\Models\Company;
+use Throwable;
 
 class CompanyService
 {
 
     /**
-     * @throws \Throwable
+     * @throws Throwable
      */
-    public function store($name)
+    public function store(string $name)
     {
         $company = new Company();
         $company->name = $name;
         $company->saveOrFail();
     }
+
+    /**
+     * @throws Throwable
+     */
+    public function update(Company $company, string $name)
+    {
+        $company->name = $name;
+        $company->saveOrFail();
+    }
+
+
+    public function index()
+    {
+        return Company::all();
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function delete(Company $company)
+    {
+        $company->deleteOrFail();
+    }
+
 }
