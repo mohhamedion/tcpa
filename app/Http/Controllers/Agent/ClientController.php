9a0b188b59e9fa01931c663aab1dd5a21b24bae3
Administrator <mohhamedion@gmail.com>
2024-12-03 19:32:15 +0300

    Add client service

--- app/Http/Controllers/Agent/ClientController.php
+++ app/Http/Controllers/Agent/ClientController.php
@@ -3,18 +3,15 @@
 namespace App\Http\Controllers\Agent;
 
 use App\Http\Controllers\Controller;
-use App\Models\CompanySmsSettings;
-use App\Services\CompanySmsSettingsService;
-use Illuminate\Http\Request;
-use Throwable;
+use App\Services\ClientService;
 
 class ClientController extends Controller
 {
-    private CompanySmsSettingsService $companySmsSettingsService;
+    private ClientService $clientService;
 
-    public function __construct(CompanySmsSettingsService $companySmsSettingsService)
+    public function __construct(ClientService $clientService)
     {
-        $this->companySmsSettingsService = $companySmsSettingsService;
+        $this->clientService = $clientService;
     }
 
 
@@ -22,28 +19,6 @@ class ClientController extends Controller
 
         return view('agent.clients.index');
     }
-    /**
-     * @throws Throwable
-     */
-    public function createForm(Request $request, CompanySmsSettings $companySmsSettings)
-    {
-        try {
-            $this->companySmsSettingsService->update($companySmsSettings, $request->input('phone_number'));
-        } catch (Throwable $exception) {
-        }
 
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function update(Request $request, CompanySmsSettings $companySmsSettings)
-    {
-        try {
-            $this->companySmsSettingsService->update($companySmsSettings, $request->input('phone_number'));
-        } catch (Throwable $exception) {
-        }
-
-    }
 
 }
