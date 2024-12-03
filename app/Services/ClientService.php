3ce814ef57eca98fa1464bf6eafbbe8abe09a147
Administrator <mohhamedion@gmail.com>
2024-12-03 19:31:16 +0300

    Add client service

---
+++ app/Services/ClientService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Client;
+use App\Models\Company;
+use App\Models\User;
+use Throwable;
+
+
+class ClientService
+{
+    /**
+     * @throws Throwable
+     */
+    public function store(Company $company, User $user, string $firstName, string $lastName, string $phoneNumber, string $language)
+    {
+        $client = new Client();
+        $client->company_id = $company->id;
+        $client->first_name = $firstName;
+        $client->last_name = $lastName;
+        $client->phone_number = $phoneNumber;
+        $client->agent_id = $user->id;
+        $client->language = $language;
+        //todo send sms
+        $client->status = 'waiting_for_verification'; //todo move to enum
+        $client->verification_code = rand(9).rand(9).rand(9).rand(9);
+        $client->saveOrFail();
+
+    }
+
+
+
+}
