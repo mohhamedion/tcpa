8974866bd8e3d2f44f93b37628f2b43315138128
Administrator <mohhamedion@gmail.com>
2024-12-03 16:23:30 +0300

    Add delete and update routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,26 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::prefix('admin')->group(function() {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/auth', [AuthController::class, 'auth'])->name('auth');
 });
 
-
 Route::middleware('auth:web')->group(function()
 {
-    Route::get('/dashboard', function ()
-    {
+    Route::get('/', function () {
         return view('admin.index');
     })->name('dashboard');
+
+    Route::group(['prefix' => 'companies'],function() {
+        Route::get('/',[CompanyController::class,'index'])->name('companies.index');
+        Route::get('/create',[CompanyController::class, 'createForm'])->name('companies.createForm');
+        Route::get('/update/{company}',[CompanyController::class,'updateForm'])->name('companies.updateForm');
+        Route::post('/store',[CompanyController::class,'store'])->name('companies.store');
+        Route::post('/update/{company}',[CompanyController::class,'update'])->name('companies.update');
+        Route::delete('/{company}',[CompanyController::class,'delete'])->name('companies.delete');
+    });
 });
