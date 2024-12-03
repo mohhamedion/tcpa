5e8f970a2315703009602156dc7d980e574dc510
Administrator <mohhamedion@gmail.com>
2024-12-03 16:42:35 +0300

    Move Company Controller to admin

--- app/Http/Controllers/Admin/CompanyController.php
+++ app/Http/Controllers/Admin/CompanyController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Company;
 use App\Services\CompanyService;
 use Illuminate\Http\Request;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\CompanyController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function() {
