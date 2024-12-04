d36dfb67d2f5e769168bcef7d783003a39ec3e21
Administrator <mohhamedion@gmail.com>
2024-12-04 17:29:10 +0300

    add relation to company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,13 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 
 /**
  * @property int $id
  * @property string $name;
+ * @property companySmsSettings smsSettings;
  */
 class Company extends Model
 {
-    //
+    public function smsSettings(): HasOne
+    {
+        return $this->hasOne(CompanySmsSettings::class,'company_id','id');
+    }
+
 }
